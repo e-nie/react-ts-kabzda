3c91c0bcea6fa0e87c9e4a5bf778a89deb4ba8b3
Eva Niehaus <eniehaus111@gmail.com>
2023-07-01 17:57:33 +0200

    reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import {Rating} from "./components/Rating";
 function App() {
       return (
         <div>
-            <PageTitle title = {'This is App component'} />
-            <PageTitle title = {'My friends'} />
-            Article 1
-            <Rating value = {3} />
+            {/*<PageTitle title = {'This is App component'} />*/}
+            {/*<PageTitle title = {'My friends'} />*/}
+
             <Accordion titleValue = "Menu" collapsed={true}/>
             <Accordion titleValue = "Users" collapsed={false}/>
-            <Rating value = {0} />
-            <Rating value = {1} />
-            <Rating value = {2} />
-            <Rating value = {3} />
-            <Rating value = {4} />
-            <Rating value = {5} />
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+
 
         </div>
     );
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -7,18 +7,13 @@ type AccordionPropsType = {
 
 
 function Accordion(props: AccordionPropsType) {
-    if (props.collapsed) {
-        return <div>
-            <AccordionTitle title = {props.titleValue} />
-            {/*<AccordionBody />*/}
-        </div>
-    }
     return <div>
         <AccordionTitle title = {props.titleValue} />
-        <AccordionBody />
+        {props.collapsed ? false : <AccordionBody   />}
     </div>
-
 }
+
+
 
 type AccordionTitlePropsType = {
     title: string
