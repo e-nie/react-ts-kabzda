2b627272f655472e7db07d1c6a00a2a22c888d7b
Eva Niehaus <eniehaus111@gmail.com>
2023-10-23 01:09:38 +0200

    controlled input, checkbox, select -  video 13

--- src/components/onOff/OnOff.stories.tsx
+++ src/components/onOff/OnOff.stories.tsx
@@ -9,6 +9,7 @@ export default {
 
 
 const callback = action('on or off clicked')
+
 export const OnMode = () => <OnOff on = {true} onChange = {callback} />
 export const OffMode = () => <OnOff on = {false} onChange = {callback} />
 
--- src/input.stories.js
+++ src/input.stories.js
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react';
+import { action } from '@storybook/addon-actions';
+
 
 export default {
     title: 'input',
@@ -34,4 +36,37 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
         actual value: { value } </>;
 };
 
-export const ControlledInputWithFixedValue = () => <input value = 'it-incubator.by' />;
+export const ControlledInput = () => {
+
+    const [parentValue, setParentValue] = useState('');
+
+    const onChange = (e) => {
+        setParentValue(e.currentTarget.value);
+    };
+    return <input value = { parentValue } onChange = { onChange } />;
+};
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true);
+    const onChange = (e) => {
+        setParentValue(e.currentTarget.checked);
+    };
+    return <input type = { 'checkbox' } checked = { parentValue } onChange = { onChange } />;
+};
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState('1');
+    const onChange = (e) => {
+        setParentValue(e.currentTarget.value);
+    };
+    return <select value={parentValue} onChange={onChange}>
+        <option >none</option>
+        <option value = '1'>Minsk</option>
+        <option value = '2'>Moscow</option>
+        <option value = '3'>Kyiv</option>
+    </select>
+
+}
+
+
+    export const ControlledInputWithFixedValue = () => <input value = 'it-incubator.by' />;
