c8f1dedc20618cc2f6f1a464d990f59829e921c0
e-nie <99044956+e-nie@users.noreply.github.com>
2023-10-29 23:29:55 +0100

    add React.memo -  video 17

--- src/input.stories.js
+++ src/input.stories.js
@@ -4,7 +4,6 @@ import { action } from '@storybook/addon-actions';
 
 export default {
     title: 'input',
-
 };
 
 export const UncontrolledInput = () => <input />;
---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'React.memo demo',
+
+};
+
+
+const NewMessagesCounter = (props: { count: number }) => {
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log('users');
+    return <div>{props.users.map((u, i) => <div key = {i}>{u}</div>)}</div>
+}
+
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    console.log('Example');
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Evchen', 'Evelyne', 'Robert', 'Mark']);
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta'  + new Date().getTime()]
+        // users.push('Sveta ' + new Date().getTime())
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick = {() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        <button onClick = { addUser}>addUser
+        </button>
+        <NewMessagesCounter count = {counter} />
+        <Users users = {users} />
+    </>
+}
